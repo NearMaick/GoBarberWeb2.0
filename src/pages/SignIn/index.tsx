e06162b2a5c27eb813cc5186cd6feec84409d735
Maick Souza <maick_a_s@msn.com>
2020-06-02 18:23:02 -0300

    refactor(web): making input & button components

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
@@ -13,11 +16,16 @@ const SignIn: React.FC = () => (
       <form action="">
         <h1>Faça seu logon</h1>
 
-        <input placeholder="E-mail" />
+        <Input type="email" name="email" icon={FiMail} placeholder="E-mail" />
 
-        <input type="password" placeholder="Senha" />
+        <Input
+          type="password"
+          name="password"
+          icon={FiLock}
+          placeholder="Senha"
+        />
 
-        <button type="submit">Entrar</button>
+        <Button>Entrar</Button>
 
         <a href="#none">Esqueci minha senha</a>
       </form>
